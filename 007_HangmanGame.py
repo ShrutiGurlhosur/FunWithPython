bfc0dab2e703e5dd08b0844c85b433fd56441c6d
ShrutiGurlhosur <shruti.gurlhosur@gmail.com>
2021-05-21 19:20:42 -0400

    Day 7: Hangman game

---
+++ 007_HangmanGame.py
@@ -0,0 +1,113 @@
+import random
+import os
+
+logo = '''
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/                       
+'''
+
+stages = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+
+#hangman word list
+word_list = ["angel","baboon","breakfast","cranberry","drive","elephant","family","gameboy","houshold"]
+lives = 6
+
+print(logo)
+print("\n\n\n")
+#chosing a random word from list to guess from
+chosen_word = random.choice((word_list))
+
+#list to show blanks and to show progress
+display = []
+for _ in chosen_word:
+    display.append("_")
+
+print(stages[len(stages)-lives-1])
+
+while "_" in display and lives > 0:
+
+    #take guess input form user
+    guess = input("Guess a letter:\n").lower()
+
+    os.system("clear")
+
+    #check for guessed letter
+    #if user already guessed the character previously
+    if guess in display:
+        print("You already guessed that letter.\n")
+    #if guessed char is not present in chosen word
+    elif guess not in chosen_word:
+        print(f"You guessed {guess}, that is not in the word. You lose a life.")
+        lives -= 1
+        #user exhausted all chances and lost
+        if lives == 0:
+            print(stages[len(stages)-lives-1])
+            print(f"{' '.join(display)}\n\n")
+            print(f"Word was: {chosen_word}")
+            print("You lose!")
+            exit(0)
+    #character is present in chosen word
+    else:
+        for position in range(len(chosen_word)):
+            if chosen_word[position] == guess:
+                display[position] = guess
+
+    print(stages[len(stages) - lives-1])
+    print(f"{' '.join(display)}\n\n")
+
+print ("You win!\n\n")
